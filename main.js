facb2901ee2e880ea92e6a8b9027e4f6b481358e
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-09-09 17:56:15 +0300

    Created grid

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";!function(e,t){const c=document.querySelector(".grid-container");for(let e=0;e<20;e++)for(let e=0;e<40;e++){const e=document.createElement("div");e.classList.add("grid-square"),c.appendChild(e)}}()})();
