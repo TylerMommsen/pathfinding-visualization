2c7a782df005429afb3f4adc5da3fb2d175fb171
Tyler Mommsen <65496518+TylerMommsen@users.noreply.github.com>
2023-09-09 20:45:43 +0300

    Configured prettier with eslint
    - Create node class
    
    - Create gridhandler file
    
    - Add site favicon

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";!function(e,t){const c=document.querySelector(".grid-container");for(let e=0;e<20;e++)for(let e=0;e<40;e++){const e=document.createElement("div");e.classList.add("grid-square"),c.appendChild(e)}}()})();
+(()=>{"use strict";!function(e,t){const c=document.querySelector(".grid-container");for(let n=0;n<e;n++)for(let e=0;e<t;e++){const e=document.createElement("div");e.classList.add("grid-square"),c.appendChild(e)}}(25,60)})();
